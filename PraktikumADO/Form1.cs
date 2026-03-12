812d8345a5637217c68f7017a41791fedfe62ef3
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-03-12 14:34:41 +0800

    koneksi & command

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -9,3 +9,14 @@ namespace PraktikumADO
         // Deklarasi Variabel Koneksi & Command
         SqlConnection conn;
         SqlCommand cmd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Opsional: Set default text
+            txtHasil.Text = "";
+        }
