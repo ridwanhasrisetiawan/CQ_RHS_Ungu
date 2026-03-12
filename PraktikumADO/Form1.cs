72cf4e41900cab0743e16ca5aab29121f4d75cb3
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-03-12 14:45:57 +0800

    event button hitung mk

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -63,3 +63,23 @@ namespace PraktikumADO
                 MessageBox.Show("Error: " + ex.Message);
             }
         }
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
